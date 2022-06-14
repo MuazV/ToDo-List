e9407bd84e9b0528bb4944a01e34775b904065ac
muaz <muaz.vakitsayan@gmail.com>
2022-06-14 19:12:25 +0300

    updated

--- app.js
+++ app.js
@@ -1,54 +1,72 @@
-window.addEventListener("load", () => {
+const input = document.querySelector("#input-area");
+const tasks = document.querySelector(".tasks");
+const Added = document.querySelector("#Added");
 
-    const input = document.querySelector("#input-area");
-    const tasks = document.querySelector(".tasks");
-    const Added = document.querySelector("#Added");
+let todos = JSON.parse(localStorage.getItem("todos"));
+if(todos == null){
+    localStorage.setItem("todos", JSON.stringify([]));
+}
+else {
+    todos.forEach((todoObject) => 
+    addHTML(todoObject)
+    )
+}
 
-    input.focus();
+function addHTML(todoObject) {
+    const task_eleman = document.createElement("div");
+    task_eleman.classList.add("task");
 
-    Added.addEventListener("click", (e) => {
-        e.preventDefault();
+    const todo_cb = document.createElement("input");
+    todo_cb.classList.add("todo_cb");
+    todo_cb.type = "checkbox";
 
-        const inputtext = input.value;
-        
-        if(!inputtext){
-            alert("Please fill this field");
-            return;
-        }
+    const task_para = document.createElement("p");
+    task_para.classList.add("text");
+    task_para.innerText = todoObject.content;
+  
 
-        const task_eleman = document.createElement("div");
-        task_eleman.classList.add("task");
+    const task_button = document.createElement("button");
+    task_button.classList.add("sil");
+    task_button.innerText = "X";
 
-        const todo_cb = document.createElement("input");
-        todo_cb.classList.add("todo_cb");
-        todo_cb.type = "checkbox";
+    task_eleman.append(todo_cb);
+    task_eleman.append(task_para);
+    task_eleman.append(task_button);
 
-        const task_para = document.createElement("p");
-        task_para.classList.add("text");
-        task_para.innerText = inputtext;
-      
-
-        const task_button = document.createElement("button");
-        task_button.classList.add("sil");
-        task_button.innerText = "X";
-
-        task_eleman.append(todo_cb);
-        task_eleman.append(task_para);
-        task_eleman.append(task_button);
-
-        tasks.append(task_eleman);
-
-        input.value = ""
-
-        task_button.addEventListener("click", () => {
-            tasks.removeChild(task_eleman)
-        })
-      
+    tasks.append(task_eleman);
+}
 
 
+Added.addEventListener("click", (e) => {
+
+    e.preventDefault();
+    const inputtext = input.value
+    if(!inputtext){
+        alert("Please fill this field");
+        return;
+    }else {
+        const todoObject = {
+        id: new Date().getTime(),
+        isDone: false,
+        content: inputtext 
+    }
+let todos = JSON.parse(localStorage.getItem("todos"));
+todos.push(todoObject);
+
+localStorage.setItem("todos", JSON.stringify(todos))
+
+    }
+
+addHTML(todoObject)
+
+    input.value = ""
+
+    task_button.addEventListener("click", () => {
+        tasks.removeChild(task_eleman)
+    })
     })
 
-
-
+window.addEventListener("load", () => {
+    input.focus();
 })
 
